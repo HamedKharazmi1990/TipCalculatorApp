16612b7fff0de70b648d879bfdcb69d7fe698ee2
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-03 15:32:57 +0100

    Initial Commit

---
+++ Tip Calculator/Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorApp.swift
+//  Tip Calculator
+//
+//  Created by Hamed Kharazmi on 03.01.26.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
