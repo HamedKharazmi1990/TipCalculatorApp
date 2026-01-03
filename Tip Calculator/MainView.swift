08894340a5f1a929361adeff9a8bf42e63767b05
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-03 21:59:08 +0100

    Added device size classes and created code snippet

---
+++ Tip Calculator/MainView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Tip Calculator
+//
+//  Created by Hamed Kharazmi on 03.01.26.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    
+    var isIPad: Bool {
+        horizontalSizeClass == .regular && verticalSizeClass == .regular
+    }
+    
+    var isPortraitPhone: Bool {
+        horizontalSizeClass == .compact && verticalSizeClass == .regular
+    }
+    
+    var body: some View {
+        VStack {
+            if isPortraitPhone {
+                Text("Tip Calculator View (portrait)")
+            } else {
+                Text("Tip Calculator View (landsape)")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
