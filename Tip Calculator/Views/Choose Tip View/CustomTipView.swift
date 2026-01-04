dd9ed242a30a8fc7d050b8ee207ac606ff661c53
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-04 20:34:36 +0100

    Setting up choose tip view

---
+++ Tip Calculator/Views/Choose Tip View/CustomTipView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTipView.swift
+//  Tip Calculator
+//
+//  Created by Hamed Kharazmi on 04.01.26.
+//
+
+import SwiftUI
+
+struct CustomTipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTipView()
+}
---
+++ Tip Calculator/Views/Choose Tip View/SelectButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectButtonView.swift
+//  Tip Calculator
+//
+//  Created by Hamed Kharazmi on 04.01.26.
+//
+
+import SwiftUI
+
+struct SelectButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectButtonView()
+}
